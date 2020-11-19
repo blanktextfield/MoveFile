f16c95e14d548657e60b64312eb43b126d42bd7d
unknown <mammutipoja1@gmail.com>
2020-11-19 17:12:43 +0200

    MoveFile

---
+++ DeleteFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileDelete();
+        }
+
+        public static void FileDelete()
+        {
+            string rootpath = @"C:\Users\mammu\samples\playerTwo";
+            string fileName = "ball.txt";
+
+            if (File.Exists(Path.Combine(rootpath, fileName)))
+            {
+                File.Delete(Path.Combine(rootpath, fileName));
+                Console.WriteLine("File deleted.");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+
+        }
+
+
+    }
+}
---
+++ MoveFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace MoveFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveFile();
+        }
+        public static void MoveFile()
+        {
+            string sourceDirectory = @"C:\Users\mammu\samples\playerOne";
+            string destinationPath = @"C:\Users\mammu\samples\playerTwo";
+            string fileName = "ball.txt";
+
+            File.Move(Path.Combine(sourceDirectory, fileName), Path.Combine(destinationPath, fileName));
+
+        }
+
+
+    }
+
+
+}
